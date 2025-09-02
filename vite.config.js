cc0491625bc737a0f388e2ae1d6a3026e296102d
Subashree <subashree.0731@gmail.com>
2025-09-02 12:24:02 +0530

    hlo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+fgfgg
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugygyghgyjgins: [react()],
 })
+hgghhghghjhj
